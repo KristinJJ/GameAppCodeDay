4d61c3d528e83d2da1d5fa1ff53a5046d86f55d7
jardiamj <1088732+jardiamj@users.noreply.github.com>
2020-08-23 23:37:41 -0700

    Created shell project using gdx-setup.jar

---
+++ SaveTheBees/android/src/com/spelunkers/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.spelunkers.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.spelunkers.game.SaveTheBees;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SaveTheBees(), config);
+	}
+}
