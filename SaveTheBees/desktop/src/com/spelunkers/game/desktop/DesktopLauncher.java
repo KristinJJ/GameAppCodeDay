70341aad138dbbd0c64a7dc0d3f2de5ef08fcdf5
Kristin Jue <kristin.jue@gmail.com>
2020-08-24 16:03:08 -0700

    reverted library to version 2 (from version 3)

--- SaveTheBees/desktop/src/com/spelunkers/game/desktop/DesktopLauncher.java
+++ SaveTheBees/desktop/src/com/spelunkers/game/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package com.spelunkers.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.spelunkers.game.SaveTheBees;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		new Lwjgl3Application(new SaveTheBees(), config);
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SaveTheBees(), config);
 	}
 }
