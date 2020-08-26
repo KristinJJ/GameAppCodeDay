163fbe619f627aa523c4ac5696e365012ba181d8
jardiamj <1088732+jardiamj@users.noreply.github.com>
2020-08-25 17:17:40 -0700

    Added button and skin to MenuScreen

--- SaveTheBees/core/src/com/spelunkers/game/screens/MenuScreen.java
+++ SaveTheBees/core/src/com/spelunkers/game/screens/MenuScreen.java
@@ -7,50 +7,90 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.spelunkers.game.BeesGame;
 
 public class MenuScreen extends ScreenAdapter {
     private BeesGame game;
     private Texture background;
-    private Texture logo;
-    private Texture playBtn;
+    //private Texture logo;
+    private Stage stage;
 
     public MenuScreen(BeesGame game) {
         this.game = game;
         background = new Texture("background.png");
-        playBtn = new Texture("worker-bee.png");
-        logo = new Texture("SaveTheBees-Logo.png");
+        //playBtn = new Texture("worker-bee.png");
+
+
     }
 
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(new InputAdapter() {
+//        Gdx.input.setInputProcessor(new InputAdapter() {
+//            @Override
+//            public boolean keyDown(int keycode) {
+//                if (keycode == Input.Keys.ENTER) {
+//                    game.setScreen(new PlayScreen(game));
+//                }
+//                return true;
+//            }
+//
+//            @Override
+//            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+//                game.setScreen(new PlayScreen(game));
+//                return true;
+//            }
+//        });
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        Skin skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
+
+        // So much work for a button...
+        Button playBtn = new TextButton("Play", skin, "small");
+        playBtn.setSize(100, 50);
+        playBtn.setPosition((BeesGame.WIDTH / 2) - (playBtn.getWidth() / 2), (BeesGame.HEIGHT / 2) - 200);
+        playBtn.setTransform(true);
+        playBtn.scaleBy(0.5f);
+        playBtn.addListener(new InputListener() {
             @Override
-            public boolean keyDown(int keycode) {
-                if (keycode == Input.Keys.ENTER) {
-                    game.setScreen(new PlayScreen(game));
-                }
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 return true;
             }
 
             @Override
-            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
                 game.setScreen(new PlayScreen(game));
-                return true;
             }
         });
+
+        // Here goes our logo
+        Texture logo = new Texture("SaveTheBees-Logo.png");
+        Image logoImage = new Image();
+        logoImage.setSize(400, 400);
+        logoImage.setPosition((BeesGame.WIDTH / 2) - (logoImage.getWidth() / 2), (BeesGame.HEIGHT / 2));
+        logoImage.setDrawable(new TextureRegionDrawable(new TextureRegion(logo)));
+
+        stage.addActor(playBtn);
+        stage.addActor(logoImage);
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0.75f, 0.25f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        SpriteBatch batch = game.getSpriteBatch();
-        batch.begin();
-        batch.draw(background, 0, 0, BeesGame.WIDTH, BeesGame.HEIGHT);
-        batch.draw(logo, (BeesGame.WIDTH / 2) - 200, (BeesGame.HEIGHT / 2) - 200, 400, 400);
-        //batch.draw(playBtn, (BeesGame.WIDTH / 2) - 25, BeesGame.HEIGHT / 2, 50, 50);
-        batch.end();
+        stage.act();
+        stage.draw();
     }
 
     @Override
@@ -61,7 +101,7 @@ public class MenuScreen extends ScreenAdapter {
     @Override
     public void dispose() {
         background.dispose();
-        playBtn.dispose();
+        //playBtn.dispose();
         game.dispose();
     }
 }
