e59e288261cb5cf1a7ed6dba80ef05f66799dcf7
jardiamj <1088732+jardiamj@users.noreply.github.com>
2020-08-25 22:58:20 -0700

    Made the bee bounce off the walls (Kind of)

--- SaveTheBees/core/src/com/spelunkers/game/screens/MenuScreen.java
+++ SaveTheBees/core/src/com/spelunkers/game/screens/MenuScreen.java
@@ -3,6 +3,7 @@ package com.spelunkers.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -34,7 +35,10 @@ public class MenuScreen extends ScreenAdapter {
 
     @Override
     public void show() {
-        stage = new Stage(new ScreenViewport());
+        OrthographicCamera camera = new OrthographicCamera(BeesGame.WIDTH, BeesGame.HEIGHT);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+
+        stage = new Stage(new ScreenViewport(camera));
         Gdx.input.setInputProcessor(stage);
 
         Skin skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
--- SaveTheBees/core/src/com/spelunkers/game/sprites/Bee.java
+++ SaveTheBees/core/src/com/spelunkers/game/sprites/Bee.java
@@ -2,60 +2,90 @@ package com.spelunkers.game.sprites;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
 
 public class Bee extends Actor {
     private Sprite sprite;
     private Circle body;
+    private ShapeRenderer shapeRenderer;
 
-    public static final int SPEED = 100;
+    public static final int SPEED = 500;
     private static final float SCALE = 0.075f;
 
     public Bee() {
         sprite = new Sprite(new Texture("worker-bee.png"));
-        sprite.setScale(SCALE);
+        sprite.setSize(sprite.getWidth() * SCALE, sprite.getHeight() * SCALE);
         sprite.setOrigin(0, 0);
         setBounds(sprite.getX(), sprite.getY(), sprite.getWidth(), sprite.getHeight());
         setTouchable(Touchable.enabled);
 
-        body = new Circle(sprite.getX() + sprite.getWidth() / 2, sprite.getY() + sprite.getHeight() / 2, sprite.getHeight() / 1.5f);
+        shapeRenderer = new ShapeRenderer();
+        body = new Circle(sprite.getX() + sprite.getWidth() / 2, sprite.getY() + sprite.getHeight() / 2, sprite.getHeight() * 0.25f);
+
     }
 
     @Override
     protected void positionChanged() {
         sprite.setPosition(getX(), getY());
+        body.setPosition(sprite.getX() + sprite.getWidth() / 2, sprite.getY() + sprite.getHeight() / 2);
         super.positionChanged();
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         sprite.draw(batch);
+
+        batch.end();
+
+        shapeRenderer.setColor(Color.BLACK);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.circle(body.x, body.y, body.radius);
+        shapeRenderer.end();
+
+        batch.begin();
+
     }
 
     @Override
     public void act(float delta) {
         // Update bee new position after motion keys
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            moveBy(-SPEED * delta, 0);
+            if (getX() > 0) {
+                moveBy(-SPEED * delta, 0);
+            } else {
+                moveBy(SPEED * delta, 0);
+            }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            moveBy(SPEED * delta, 0);
+            if (getX() < getStage().getWidth() - sprite.getWidth()) {
+                moveBy(SPEED * delta, 0);
+            } else {
+                moveBy(-SPEED * delta, 0);
+            }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            moveBy(0, SPEED * delta);
+            if (getY() < getStage().getHeight() - sprite.getHeight()) {
+                moveBy(0, SPEED * delta);
+            } else {
+                moveBy(0, -SPEED * delta);
+            }
+
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            moveBy(0, -SPEED * delta);
+            if (getY() > 0) {
+                moveBy(0, -SPEED * delta);
+            } else {
+                moveBy(0, SPEED * delta);
+            }
         }
+
         super.act(delta);
     }
 
