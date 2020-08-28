a6ae611a63f06f02cf95b48e60da54904a5b09d7
Kristin Jue <kristin.jue@gmail.com>
2020-08-27 22:03:19 -0700

    CreditScreen and InstructionScreen graphics changed from magic numbers to constant conversions

--- SaveTheBees/core/src/com/spelunkers/game/screens/EndScreen.java
+++ SaveTheBees/core/src/com/spelunkers/game/screens/EndScreen.java
@@ -111,7 +111,7 @@ public class EndScreen extends ScreenAdapter {
         //Honey picture
         Texture honey = new Texture("honey.png");
         Image honeyImage = new Image();
-        honeyImage.setSize(BeesGame.WIDTH, 300);
+        honeyImage.setSize(BeesGame.WIDTH, (BeesGame.HEIGHT / 10) * 3);
         honeyImage.setPosition(0f,BeesGame.HEIGHT - honeyImage.getHeight());
         honeyImage.setDrawable(new TextureRegionDrawable(new TextureRegion(honey)));
 
